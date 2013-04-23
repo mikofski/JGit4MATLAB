8925411b34c8a7ce08a18edc1e6384225241836a
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2013-04-22 19:29:42 -0700

    use files instead of pathlist

--- add.m
+++ add.m
@@ -1,13 +1,12 @@
-function add(pathlist,varargin)
+function add(files,varargin)
 %ADD Add files to the index.
-%   JGIT.ADD(PATHLIST) adds the file given by the string PATHLIST to the index.
-%   JGIT.ADD(PATHLIST) adds the files given by the cell string PATHLIST to
-%   the index.
-%   JGIT.ADD(PATHLIST,PARAM,VAL,...) any of the following parameter-value
+%   JGIT.ADD(FILES) adds the file or files given by the string or cell-string
+%   FILES to the index.
+%   JGIT.ADD(FILES,PARAM,VAL,...) any of the following parameter-value
 %   pairs PARAM,VAL can be specified.
-%   update <logical> [false] Only stage tracked files. New files will not
-%   be added to the index.
-%   gitDir <char> [PWD] Add to index of the repo in specified folder.
+%       update <logical> [false] Only stage tracked files. New files will not
+%           be added to the index.
+%       gitDir <char> [PWD] Add to index of the repo in specified folder.
 %
 %   For more information see also
 %   <a href="https://www.kernel.org/pub/software/scm/git/docs/git-add.html">Git Add Documentation</a>
@@ -25,20 +24,20 @@ function add(pathlist,varargin)
 
 %% Check inputs
 p = inputParser;
-p.addRequired('pathlist',@(x)validatepathlist(pathlist))
+p.addRequired('files',@(x)validatefiles(files))
 p.addParamValue('update',false,@(x)validateattributes(x,{'logical'},{'scalar'}))
 p.addParamValue('gitDir',pwd,@(x)validateattributes(x,{'char'},{'row'}))
-p.parse(pathlist,varargin{:})
+p.parse(files,varargin{:})
 gitDir = p.Results.gitDir;
 gitAPI = JGit.getGitAPI(gitDir);
 addCMD = gitAPI.add;
 %% add files
-if iscellstr(p.Results.pathlist)
-    for n = 1:numel(pathlist)
-        addCMD.addFilepattern(p.Results.pathlist{n});
+if iscellstr(p.Results.files)
+    for n = 1:numel(files)
+        addCMD.addFilepattern(p.Results.files{n});
     end
-elseif ischar(p.Results.pathlist)
-    addCMD.addFilepattern(p.Results.pathlist);
+elseif ischar(p.Results.files)
+    addCMD.addFilepattern(p.Results.files);
 end
 %% set update
 if p.Results.update
@@ -48,10 +47,10 @@ end
 addCMD.call;
 end
 
-function tf = validatepathlist(pathlist)
-if iscellstr(pathlist)
+function tf = validatefiles(files)
+if iscellstr(files)
     tf = true;
     return
 end
-validateattributes(pathlist,{'char'},{'row'})
+validateattributes(files,{'char'},{'row'})
 end
