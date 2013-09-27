88ec56c978495b6365004c2d0b643bf22ecb615f
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2013-09-27 04:45:32 -0700

    finish push cmd

--- @JGit/push.m
+++ @JGit/push.m
@@ -1,8 +1,8 @@
 function push(ref,varargin)
 %JGIT.PUSH Push commit with current head.
-%   JGIT.PUSH(REF) sets references and ID's of commits to push.
-%   JGIT.PUSH(REF,PARAMETER,VALUE,...) uses any combination of the
+%   JGIT.PUSH(PARAMETER,VALUE,...) uses any combination of the
 %   following PARAMETER, VALUE pairs.
+%   'ref' <char> sets references and ID's of commits to push.
 %   'setDryRun' <logical> Sets a dry run.
 %   'setForce' <logical> Sets force push.
 %   'setPushAll' <logical> Push all branches.
@@ -17,10 +17,10 @@ function push(ref,varargin)
 %   <a href="http://download.eclipse.org/jgit/docs/latest/apidocs/org/eclipse/jgit/transport/PushResult.html">JGit Git API Class PushResult</a>
 %
 %   Example:
-%       JGIT.PUSH('feature') % push 'feature' ref to default remote
-%       JGIT.PUSH('fad9663b23d59332fd5387ba6f506c54167a6707')
-%       JGIT.PUSH('feature', 'setRemote', 'upstream') % push upstream
-%       JGIT.PUSH('feature','setForce',true) % force push master to default
+%       JGIT.PUSH('ref','feature') % push 'feature' ref to default remote
+%       JGIT.PUSH('ref','fad9663b23d59332fd5387ba6f506c54167a6707')
+%       JGIT.PUSH('ref','feature', 'setRemote', 'upstream') % push upstream
+%       JGIT.PUSH('ref','feature','setForce',true) % force push master to default
 %
 %   See also JGIT
 %
@@ -30,59 +30,48 @@ function push(ref,varargin)
 
 %% check inputs
 p = inputParser;
-p.addRequired('ref',@(x)validateattributes(x,{'char'},{'row'}))
+p.addParamValue('ref','',@(x)validateattributes(x,{'char'},{'row'}))
 p.addParamValue('setDryRun',false,@(x)validateattributes(x,{'logical'},{'scalar'}))
 p.addParamValue('setForce',false,@(x)validateattributes(x,{'logical'},{'scalar'}))
 p.addParamValue('setPushAll',false,@(x)validateattributes(x,{'logical'},{'scalar'}))
 p.addParamValue('setPushTags',false,@(x)validateattributes(x,{'logical'},{'scalar'}))
 p.addParamValue('progressMonitor',com.mikofski.jgit4matlab.MATLABProgressMonitor,@(x)isjava(x))
-p.addParamValue('setRemote','',@(x)validateattributes(x,{'char'},{'row'}))
+p.addParamValue('remote','',@(x)validateattributes(x,{'char'},{'row'}))
 p.addParamValue('gitDir',pwd,@(x)validateattributes(x,{'char'},{'row'}))
 p.parse(ref,varargin{:})
 gitDir = p.Results.gitDir;
 gitAPI = JGit.getGitAPI(gitDir);
 pushCMD = gitAPI.push;
 %% repository
-repo = gitAPI.getRepository;
+% repo = gitAPI.getRepository;
 %% add ref
-pushCMD.add(repo.resolve(p.Results.ref));
-%% set fast forward mode
-if ~isempty(p.Results.fastForward)
-    switch upper(p.Results.fastForward)
-        case 'FF'
-            pushCMD.setFastForward(FF);
-        case 'FF_ONLY'
-            pushCMD.setFastForward(FF_ONLY);
-        case 'NO_FF'
-            pushCMD.setFastForward(NO_FF);
-        otherwise
-            error('jgit:push:badFFmode', ...
-                'Stages are ''FF'', ''FF_ONLY'' or ''NO_FF''.')
-    end
+if ~isempty(p.Results.ref)
+    pushCMD.add(p.Results.ref);
 end
-%% set squash
-if p.Results.squash
-    pushCMD.setSquash(true);
+%% set dry run
+if p.Results.setDryRun
+    pushCMD.setDryRun(true);
 end
-%% set fast forward mode
-if ~isempty(p.Results.strategy)
-    switch upper(p.Results.strategy)
-        case 'OURS'
-            pushCMD.setStrategy(OURS);
-        case 'RESOLVE'
-            pushCMD.setStrategy(RESOLVE);
-        case 'SIMPLE_TWO_WAY_IN_CORE'
-            pushCMD.setStrategy(SIMPLE_TWO_WAY_IN_CORE);
-        case 'THEIRS'
-            pushCMD.setStrategy(THEIRS);
-        otherwise
-            error('jgit:push:badMergeStrategy', ...
-                'Stages are ''OURS'', ''RESOLVE'', ''SIMPLE_TWO_WAY_IN_CORE'' or ''THEIRS''.')
-    end
+%% set force
+if p.Results.setForce
+    pushCMD.setForce(true);
 end
-%% call CMD
-mergeResult = pushCMD.call;
-if nargout>0
-    results = mergeResult;
+%% set push all
+if p.Results.setPushAll
+    pushCMD.setPushAll(true);
 end
+%% set push tags
+if p.Results.setPushTags
+    pushCMD.setPushTags(true);
+end
+%% set progressMonitor
+pushCMD.setProgressMonitor(p.Results.progressMonitor);
+%% set remote
+if ~isempty(p.Results.remote)
+    pushCMD.setRemote(p.Results.remote);
+end
+%% call
+com.mikofski.jgit4matlab.UserInfoSshSessionFactory;
+disp(org.eclipse.jgit.transport.SshSessionFactory.getInstance)
+pushCMD.call;
 end
