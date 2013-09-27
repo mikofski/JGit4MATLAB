1dd9b3583bde102c6bb01a719babd20b3d2d7809
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2013-09-27 15:56:41 -0700

    use GRAMMAR & argparse or bruteforce?

--- jgit.m
+++ jgit.m
@@ -26,30 +26,79 @@ function jgit(varargin)
 %
 %       >> jgit commit all true message 'commit all tracked but not staged'
 
+GRAMMAR = {'add',{'files'},{'update'}; ...
+    'branch',{'cmd','newName'},{'force','startPoint','upstreamMode','listMode','oldNames'}; ...
+    'checkout',{},{}; ...
+    'clone',{},{}; ...
+    'commit',{},{}; ...
+    'diff',{},{}; ...
+    'fetch',{},{}; ...
+    'init',{},{}; ...
+    'log',{},{}; ...
+    'merge',{},{}; ...
+    'pull',{},{}; ...
+    'push',{},{}; ...
+    'status',{},{}};
 if nargin==0
     try
         JGit %#ok<NOPRT>
     catch ME
         rethrow(ME)
     end
-elseif strcmpi(varargin{1},'help')
-    fstr = 'JGit';
-    if nargin==2
-        fstr = [fstr,'.',varargin{2}];
-    end
-    help(fstr)
-else
-    bools = [false,strcmpi('true',varargin(2:end))];
-    [varargin{bools}] = deal(true);
-    bools = [false,strcmpi('false',varargin(2:end))];
-    [varargin{bools}] = deal(false);
-    numbers = [NaN,str2double(varargin(2:end))];
-    varargin(~isnan(numbers)) = num2cell(numbers(~isnan(numbers)));
-    try
-        JGit.(varargin{1})(varargin{2:end})
-    catch ME
-        rethrow(ME)
-    end
+    return
 end
+% elseif strcmpi(varargin{1},'help')
+%     fstr = 'JGit';
+%     if nargin==2
+%         fstr = [fstr,'.',varargin{2}];
+%     end
+%     help(fstr)
+% else
+cmd = varargin{1}; % command
+try
+    argopts = varargin{2:end};
+catch
+    argopts = {};
+end
+switch cmd
+    case 'help'
+        fstr = 'JGit';
+        if nargin==2
+            fstr = [fstr,'.',varargin{2}];
+        end
+        help(fstr)
+    case 'add'
+end
+try
+    %         JGit.(varargin{1})(varargin{2:end})
+    JGit.(cmd)(argparse(cmd,argopts,GRAMMAR))
+catch ME
+    rethrow(ME)
+end
+% end
+end
+
+function argparse(cmd,argopts,grammar)
+    argout = {};
+    %% look for command
+    commands = grammar(:,1);
+    icmd = strcmpi(cmd,commands); % index of command
+    if ~any(icmd)
+        error('jgit:noCommand','%s is not a valid command',cmd)
+    end
+    cmdargs = grammar(icmd,2); % command args
+    cmdopts = grammar(icmd,3); % command opts
+    %% look for option terminator '--'
+    lastopt = strcmp('--',argopts); % last option
+    if any(lastopt)
+        args = argopts(1:
+        
+    Nargs = numel(argopts); % number of args
+    for n = 1:Nargs
+        % check the first character
+        if strcmp('-',argopts{n}(1))
+            opt = strncmpi(
+        end
+    end
 end
 
