a05d3df450148da97a27911b00aec9f50b3def73
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2013-10-02 12:31:58 -0700

    use parseOpts in parseCommit

--- private/parseCommit.m
+++ private/parseCommit.m
@@ -3,46 +3,37 @@ function parsed_argopts = parseCommit(argopts)
 %   Copyright (c) 2013 Mark Mikofski
 parsed_argopts = {};
 %% options
-% all
-allcommit = strcmp('-a',argopts) | strcmp('--all',argopts);
-% amend
-amend = strcmp('--amend',argopts);
-% message
-message = strcmp('-m',argopts) | strcmp('--message',argopts);
-if any(message)
-    msgContent = argopts(circshift(message,[0,1])); % store message content
-end
-% author
-author = strcmp('--author',argopts);
-if any(author)
-    % store message content, split author name and email
-    authorNameEmail = regexp(argopts(circshift(author,[0,1])),'(.+) <(.+)>','tokens');
-    assert(~isempty(authorNameEmail),'jgit:parseCommit', ...
-        'Specify an explicit author using the standard A U Thor <author@example.com> format.')
-    authorNameEmail = authorNameEmail{1}; % extract the tokens, they are nested cells
-end
-%% pop argopts
-argopts(allcommit | amend | message | circshift(message,[0,1]) | author | ...
-    circshift(author,[0,1])) = [];
+dictionary = { ...
+    'all',{'-a','--all'},true; ...
+    'amend',{'--amend'},true; ...
+    'message',{'-m','--message'},false; ...
+    'author',{'--author'},false; ...
+    };
+[options,argopts] = parseOpts(argopts,dictionary);
 %% other options
 % filter other options and/or double-hyphen
 argopts = filterOpts(argopts);
 %% parse
 % no argument or option checks - jgit checks args/opts
 % all
-if any(allcommit)
+if options(1).('all')
     parsed_argopts = [parsed_argopts,'all',true];
 end
 % amend
-if any(amend)
+if options(1).('amend')
     parsed_argopts = [parsed_argopts,'amend',true];
 end
 % message
-if any(message)
-    parsed_argopts = [parsed_argopts,'message',msgContent];
+if options(1).('message')
+    parsed_argopts = [parsed_argopts,'message',options(2).('message')];
 end
 % author
-if any(author)
+if options(1).('author')
+    % store message content, split author name and email
+    authorNameEmail = regexp(options(2).('author'),'(.+) <(.+)>','tokens');
+    assert(~isempty(authorNameEmail),'jgit:parseCommit', ...
+        'Specify an explicit author using the standard A U Thor <author@example.com> format.')
+    authorNameEmail = authorNameEmail{1}; % extract the tokens, they are nested cells
     parsed_argopts = [parsed_argopts,'author',authorNameEmail];
 end
 % only files on command line
